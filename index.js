a8018da09a7c730379ae26ea09f358b3e7d2d11f
Juan Ramos <ptjuanramos@gmail.com>
2021-02-22 20:48:43 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const eris = require('eris');
+const bot = new eris.Client('ODEzNTAzMzcwNDUwMzA1MDI1.YDQQDQ.okYKDbJE_GXQlTI6EO7iNg1N3oA');
+let theFinalDate = new Date("12/Mar/2021 18:44:00");
+let suffixes = [
+    "Coño si...", 
+    "Joder, que pesado,", 
+    "Tonto cagón,", 
+    "Porque eres tan pesado,",
+    "Sin condiciones,",
+    "Muy bien, borroso siguiente,",
+    "Afsxafsxafsxafsx,"
+];
+
+function getRandomSuffix() {
+    return suffixes[Math.floor(Math.random() * suffixes.length)];
+}
+
+function getMessage() {
+    let currentDate = new Date();
+    let diff = Math.abs(currentDate.getTime() - theFinalDate.getTime());
+    let numberOfDays = Math.floor(diff / (1000 * 60 * 60 * 24));
+
+    return `${getRandomSuffix()} te queda como ${numberOfDays} dias joder.`
+}
+
+bot.on('messageCreate', async (msg) => {
+    const botWasMentioned = msg.mentions.find(
+        mentionedUser => mentionedUser.id === bot.user.id,
+    );
+ 
+    if (botWasMentioned) {
+        try {
+            await msg.channel.createMessage(getMessage());
+        } catch (err) {
+            console.warn('Failed to respond to mention.');
+            console.warn(err);
+        }
+    }
+});
+ 
+bot.on('error', err => {
+    console.warn(err);
+});
+ 
+bot.connect();
